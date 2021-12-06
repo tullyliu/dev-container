50b4ee23847f090020a89e1ab8f150090efe17fb
liutao04 <liutao04@baidu.com>
2021-12-06 19:43:53 +0800

    Update projector-idea.sh

--- .devcontainer/library-scripts/projector-idea.sh
+++ .devcontainer/library-scripts/projector-idea.sh
@@ -64,7 +64,7 @@ if [ ! -d "${PROJECTOR_DIR}" ]; then
     usermod -a -G projector ${USERNAME}
     umask 0002
     # Install 
-    mkdir -p ${PROJECTOR_DIR}/projector-server ${PROJECTOR_DIR}/download
+    mkdir -p ${PROJECTOR_DIR}/projector-server ${PROJECTOR_DIR}/download ${PROJECTOR_DIR}/ide
     git clone --depth=1 \
         -c core.eol=lf \
         -c core.autocrlf=false \
